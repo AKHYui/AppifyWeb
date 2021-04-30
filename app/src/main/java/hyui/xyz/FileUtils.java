ce70851a2ad810bb17fe0d38c28d72af438ea33c
AKHYui <zzk4093977@gmail.com>
2021-04-30 11:54:17 +0800

    20210430 计划使用自有保存路径

---
+++ app/src/main/java/hyui/xyz/FileUtils.java
@@ -0,0 +1,8 @@
+package hyui.xyz;
+
+public class FileUtils {
+    private static final String TAG = "FileUtils";
+    public static final int SUCCESS_FLAG = 1; //成功
+    public static final int EXITS_FLAG = 2; //已存在
+    public static final int FAILED_FLAG = 3; //失败
+}
