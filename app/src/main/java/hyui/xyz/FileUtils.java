17e72194fab7f0121938441b1b9eeb38d9e6c57b
AKHYui <zzk4093977@gmail.com>
2021-04-30 12:56:59 +0800

    20210430 FlieUtils工具类

--- app/src/main/java/hyui/xyz/FileUtils.java
+++ app/src/main/java/hyui/xyz/FileUtils.java
@@ -1,8 +1,31 @@
 package hyui.xyz;
 
+import android.util.Log;
+
+import java.io.File;
+
 public class FileUtils {
     private static final String TAG = "FileUtils";
     public static final int SUCCESS_FLAG = 1; //成功
     public static final int EXITS_FLAG = 2; //已存在
     public static final int FAILED_FLAG = 3; //失败
+
+    public static int CreateDir(String Path){
+
+        File dir = new File(Path);
+        if(dir.exists()){
+            Log.w(TAG,"该目录[" + Path + "]已存在");
+            return EXITS_FLAG;
+        }
+        if (!Path.endsWith(File.separator)){
+            Path = Path + File.separator;
+        }
+        if(dir.mkdirs()){
+            Log.d(TAG,"创建[" + Path + "]成功");
+            return SUCCESS_FLAG;
+        }
+
+        Log.e(TAG,"创建[" + Path + "]失败");
+        return FAILED_FLAG;
+    }
 }
