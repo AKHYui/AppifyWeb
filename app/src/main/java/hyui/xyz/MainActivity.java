1d5602cf71f7363c13e60e5d640c38c1aac4b9c0
AKHYui <zzk4093977@gmail.com>
2021-05-04 22:48:39 +0800

    20210504_1 新增启动界面

--- app/src/main/java/hyui/xyz/MainActivity.java
+++ app/src/main/java/hyui/xyz/MainActivity.java
@@ -40,6 +40,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //requestWindowFeature(Window.FEATURE_NO_TITLE);//去掉标题栏
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         setContentView(R.layout.activity_main);
         //找到WebView的ID
         webview = (WebView) findViewById(R.id.webView1);
